f91f1921ef7b287d7fb15db0198d61e299051ce1
PETR_MARCHANKA <pmarchanka@i-deasoft.com>
2020-05-17 14:15:48 +0300

    add shoping list screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ShoppingListScreen from './screens/ShoppingListScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Shoping list" component={ShoppingListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/ShoppingListScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ShoppingListScreen = () => {
+  return (
+    <View>
+      <Text>Shopping list</Text>
+    </View>
+  );
+};
+
+export default ShoppingListScreen;
+
+const styles = StyleSheet.create({});
